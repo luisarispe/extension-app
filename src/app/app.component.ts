1e8e99d49e09bceedc4629810b7c5efdde6110dd
luisarispe <luis.arispe1991@gmail.com>
2023-07-01 11:54:33 -0400

    finalización extensión app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'extension-app';
+  color = '#ffffff';
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+    });
+  }
+  public colorize() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id! },
+        func: updateBackgroundColor,
+        args: [this.color],
+      });
+    });
+  }
+  public updateColor(color: string) {
+    chrome.storage.sync.set({ color });
+  }
 }
+
+const updateBackgroundColor = (color: string) =>
+  (document.body.style.backgroundColor = color);
---
+++ src/background.ts
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' });
+  chrome.webNavigation.onCompleted.addListener(
+    () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id) {
+          chrome.action.disable(id);
+        }
+      });
+    },
+    { url: [{ hostContains: 'google.com' }] }
+  );
+});
